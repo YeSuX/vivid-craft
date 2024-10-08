a5c98c957a76332dd878417e5e18471991f5e968
YeSuX <351490949@qq.com>
2024-10-09 07:11:43 +0800

    feat: custom control style

--- features/editor/hooks/use-editor.ts
+++ features/editor/hooks/use-editor.ts
@@ -1,41 +1,50 @@
 import { useCallback } from "react";
-import * as fabric  from "fabric";
+import * as fabric from "fabric";
 
 export const useEditor = () => {
 
     // this is the function that will be used to initialize the editor
     const init = useCallback((
-        { 
+        {
             initialCanvas,
-            initialContainer 
+            initialContainer
         }: {
             initialCanvas: fabric.Canvas;
             initialContainer: HTMLDivElement;
         }
     ) => {
         console.log("init");
-        // this is the function that will be used to initialize the editor
-        // TODO: add the ability to add custom controls to the editor
-        fabric.Object.prototype.set({
-            cornerColor:'red',
-            cornerStyle:'circle',
-            borderColor:'red',
-            borderScaleFactor:2,
-            transparentCorners:false,
-            borderOpacityWhenMoving:0.8,
-            cornerStrokeColor:'red',
-        })
 
+        // this is the function that will be used to initialize the editor
+        // TODO: this can't work, need to find a way to set the cornerStyle to circle for all objects
+        // fabric.Object.prototype.set({
+        //     cornerColor: "#FFF",
+        //     cornerStyle: "circle",
+        //     borderColor: "#3b82f6",
+        //     borderScaleFactor: 1.5,
+        //     transparentCorners: false,
+        //     borderOpacityWhenMoving: 1,
+        //     cornerStrokeColor: "#3b82f6",
+        // });
+
+
+        const test = new fabric.Rect({
+            width: 100,
+            height: 100,
+            fill: 'red',
+            top: 100,
+            left: 100,
+        })
         const initialWorkspace = new fabric.Rect({
-            width:900,
-            height:1200,
-            name:'clip',
-            fill:'white',
-            selectable:false,
-            hasControls:false,
-            shadow:new fabric.Shadow({
-                color:'rgba(0,0,0,0.8)',
-                blur:5,
+            width: 900,
+            height: 1200,
+            name: 'clip',
+            fill: 'white',
+            selectable: false,
+            hasControls: false,
+            shadow: new fabric.Shadow({
+                color: 'rgba(0,0,0,0.8)',
+                blur: 5,
             })
         })
 
@@ -45,15 +54,9 @@ export const useEditor = () => {
         initialCanvas.add(initialWorkspace)
         initialCanvas.centerObject(initialWorkspace)
         initialCanvas.clipPath = initialWorkspace
-        
-        const test = new fabric.Rect({
-            width:100,
-            height:100,
-            fill:'red',
-            top:100,
-            left:100,
-        })
-        
+
+
+
         initialCanvas.add(test)
         initialCanvas.centerObject(test)
     }, [])
