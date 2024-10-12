b784da5900ea7fdd82b7f2bba8573ea798232775
YeSuX <351490949@qq.com>
2024-10-12 18:21:56 +0800

    feat: add dropdown-menu & react-icons

--- features/editor/components/logo.tsx
+++ features/editor/components/logo.tsx
@@ -4,12 +4,12 @@ import Link from "next/link"
 const Logo = () => {
   return (
     <Link href='/'>
-        <div>
+        <div className=" size-8 relative shrink-0">
             <Image
                 src='/logo.svg'
                 fill
                 alt="Image AI"
-            
+                className=" shrink-0 hover:opacity-75 transition"
             />
         </div>
     </Link>
