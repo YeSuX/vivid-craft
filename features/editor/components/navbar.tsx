11526b881afd948d39f464796b88046297f2e930
YeSuX <351490949@qq.com>
2024-10-12 20:57:36 +0800

    feat: add navbar item

--- features/editor/components/navbar.tsx
+++ features/editor/components/navbar.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import Hint from "@/components/hint"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Separator } from "@/components/ui/separator"
 import Logo from "@/features/editor/components/logo"
-import { ChevronDown, MousePointerClick } from "lucide-react"
+import { ChevronDown, Download, MousePointerClick, Redo2, Undo2 } from "lucide-react"
+import { BsCloudCheck } from "react-icons/bs"
 import { CiFileOn } from "react-icons/ci"
 
 const Navbar = () => {
@@ -34,10 +36,77 @@ const Navbar = () => {
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
-                <Separator orientation='vertical' className=" mx-2"/>
-                <Button variant='ghost' size={'icon'} onClick={()=>{}}>
-                    <MousePointerClick className="size-4"/>
-                </Button>
+                <Separator orientation='vertical' className=" mx-2" />
+                <Hint label="Select" side="bottom" sideOffset={10}>
+                    <Button variant='ghost' size={'icon'} onClick={() => { }}>
+                        <MousePointerClick className="size-4" />
+                    </Button>
+                </Hint>
+                <Hint label="Undo" side="bottom" sideOffset={10}>
+                    <Button variant='ghost' size={'icon'} onClick={() => { }}>
+                        <Undo2 className="size-4" />
+                    </Button>
+                </Hint>
+                <Hint label="Redo" side="bottom" sideOffset={10}>
+                    <Button variant='ghost' size={'icon'} onClick={() => { }}>
+                        <Redo2 className="size-4" />
+                    </Button>
+                </Hint>
+                <Separator orientation='vertical' className=" mx-2" />
+                <div className=" flex items-center gap-x-2">
+                    <BsCloudCheck className=" size-[20px] text-muted-foreground" />
+                    <div className=" text-xs text-muted-foreground">
+                        Saved
+                    </div>
+                </div>
+                <div className=" ml-auto flex items-center gap-x-4">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button size={'sm'} variant={'ghost'}>
+                                Export
+                                <Download className=" size-4 ml-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end" className="min-w-60">
+                            <DropdownMenuItem className=" flex items-center gap-x-2">
+                                <CiFileOn className=" size-6" />
+                                <div>
+                                    <p>JSON</p>
+                                    <p className=" text-xs text-muted-foreground">
+                                        Save for later editing
+                                    </p>
+                                </div>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className=" flex items-center gap-x-2">
+                                <CiFileOn className=" size-6" />
+                                <div>
+                                    <p>PNG</p>
+                                    <p className=" text-xs text-muted-foreground">
+                                        Best for sharing on the web
+                                    </p>
+                                </div>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className=" flex items-center gap-x-2">
+                                <CiFileOn className=" size-6" />
+                                <div>
+                                    <p>JPG</p>
+                                    <p className=" text-xs text-muted-foreground">
+                                        Save for printing
+                                    </p>
+                                </div>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className=" flex items-center gap-x-2">
+                                <CiFileOn className=" size-6" />
+                                <div>
+                                    <p>SVG</p>
+                                    <p className=" text-xs text-muted-foreground">
+                                        Save for editing in vector software
+                                    </p>
+                                </div>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </div>
         </div>
     )
